aafd1391675e47017991895565ffa4161d4a5b57
VitorCampos28 <vito.r.natal@hotmail.com>
2020-04-06 17:46:32 -0300

    Exercicio de Fixacao de Metodos Abstratos

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioFixacaoMetodosAbstratos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
